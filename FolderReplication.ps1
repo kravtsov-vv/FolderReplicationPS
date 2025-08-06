3bca25aaa70c5c14d2c91921375552604e2278eb
kravtsov-vv <kravtsov.vv@gmail.com>
2025-08-06 10:35:38 +0200

    Update FolderReplication.ps1

--- FolderReplication.ps1
+++ FolderReplication.ps1
@@ -20,7 +20,7 @@ Specifies the path to the log file.
 If only a folder is provided, the default log file name is 'FolderReplication.log'.
 If left empty, operations will be logged to the console only.
 -VerboseLog
-Include in log additional info. If param not used, only file/folder oprepations logged.
+Include in log additional info. If param not used, only file/folder operations logged.
 -MaxRetries
 Specifies the number of retry attempts for copying an item.
 Default: 5
